42078480044c7a0193c7040f25f99cce08992c9a
Sameer Gupta <sameergupta4873@gmail.com>
2023-04-26 14:51:18 +0530

    hero section completed

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ document.querySelectorAll(".reveal").forEach(function (elem) {
     elem.appendChild(parent);
 });
 
+document.querySelectorAll(".revealUp").forEach(function (elem) {
+    let parent = document.createElement("span");
+    let child = document.createElement("span");
+
+    parent.classList.add("parentUp");
+    child.classList.add("childUp");
+
+    child.innerHTML = elem.innerHTML;
+    parent.appendChild(child);
+
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+});
+
+
 // script end
 
 // gsap start
@@ -20,8 +35,9 @@ var tl = gsap.timeline();
 tl
     .from(".child span", {
         x: 100,
-        stagger: .1,
-        duration: 1.4,
+        stagger: .05,
+        duration: 0.75,
+        delay: 1,
         ease: Power3.easeInOut,
     })
     .to(".parent .child", {
@@ -46,6 +62,9 @@ tl
         delay: -0.65,
         ease: Expo.easeInOut
     })
+    .to(".parentUp .childUp", {
+        height:'20vh',
+        duration: 0.75,
+        ease: Expo.easeInOut
+    })
 // gsap end
-
-// script start
