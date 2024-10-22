067f6c51418e98ce38b223de5e5c4a44a2e10314
sameerishaare <sameergupta4873@gmail.com>
2024-10-22 14:00:16 +0530

    html web

--- script.js
+++ script.js
@@ -1,133 +1,137 @@
-var magnets = document.querySelectorAll('.magnetic')
-var strength = 50
+var magnets = document.querySelectorAll(".magnetic");
+var strength = 50;
 
-magnets.forEach( (magnet) => {
-  magnet.addEventListener('mousemove', moveMagnet );
-  magnet.addEventListener('mouseout', function(event) {
-    TweenMax.to( event.currentTarget, 1, {x: 0, y: 0, ease: Power4.easeOut})
-  } );
+magnets.forEach((magnet) => {
+  magnet.addEventListener("mousemove", moveMagnet);
+  magnet.addEventListener("mouseout", function (event) {
+    TweenMax.to(event.currentTarget, 1, { x: 0, y: 0, ease: Power4.easeOut });
+  });
 });
 
 function moveMagnet(event) {
-  var magnetButton = event.currentTarget
-  var bounding = magnetButton.getBoundingClientRect()
+  var magnetButton = event.currentTarget;
+  var bounding = magnetButton.getBoundingClientRect();
 
   //console.log(magnetButton, bounding)
 
-  TweenMax.to( magnetButton, 1, {
-    x: ((( event.clientX - bounding.left)/magnetButton.offsetWidth) - 0.5) * strength,
-    y: ((( event.clientY - bounding.top)/magnetButton.offsetHeight) - 0.5) * strength,
-    ease: Power4.easeOut
-  })
+  TweenMax.to(magnetButton, 1, {
+    x:
+      ((event.clientX - bounding.left) / magnetButton.offsetWidth - 0.5) *
+      strength,
+    y:
+      ((event.clientY - bounding.top) / magnetButton.offsetHeight - 0.5) *
+      strength,
+    ease: Power4.easeOut,
+  });
 
   //magnetButton.style.transform = 'translate(' + (((( event.clientX - bounding.left)/(magnetButton.offsetWidth))) - 0.5) * strength + 'px,'+ (((( event.clientY - bounding.top)/(magnetButton.offsetHeight))) - 0.5) * strength + 'px)';
 }
 
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
 
-const sliderContainer = document.querySelector('.slider-container')
-const slideRight = document.querySelector('.right-slide')
-const slideLeft = document.querySelector('.left-slide')
-const upButton = document.querySelector('.up-button')
-const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelectorAll('div').length
+let activeSlideIndex = 0;
 
-let activeSlideIndex = 0
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
-
-upButton.addEventListener('click', () => changeSlide('up'))
-downButton.addEventListener('click', () => changeSlide('down'))
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
 
 const changeSlide = (direction) => {
-    const sliderHeight = sliderContainer.clientHeight
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1) {
-            activeSlideIndex = 0
-        }
-    } else if(direction === 'down') {
-        activeSlideIndex--
-        if(activeSlideIndex < 0) {
-            activeSlideIndex = slidesLength - 1
-        }
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
     }
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
 
-    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
-}
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
 
 //  ijsabuhbugzxsubsa
 document.querySelectorAll(".reveal").forEach(function (elem) {
-    let parent = document.createElement("span");
-    let child = document.createElement("span");
+  let parent = document.createElement("span");
+  let child = document.createElement("span");
 
-    parent.classList.add("parent");
-    child.classList.add("child");
+  parent.classList.add("parent");
+  child.classList.add("child");
 
-    child.innerHTML = elem.innerHTML;
-    parent.appendChild(child);
+  child.innerHTML = elem.innerHTML;
+  parent.appendChild(child);
 
-    elem.innerHTML = "";
-    elem.appendChild(parent);
+  elem.innerHTML = "";
+  elem.appendChild(parent);
 });
 
 document.querySelectorAll(".revealUp").forEach(function (elem) {
-    let parent = document.createElement("span");
-    let child = document.createElement("span");
+  let parent = document.createElement("span");
+  let child = document.createElement("span");
 
-    parent.classList.add("parentUp");
-    child.classList.add("childUp");
+  parent.classList.add("parentUp");
+  child.classList.add("childUp");
 
-    child.innerHTML = elem.innerHTML;
-    parent.appendChild(child);
+  child.innerHTML = elem.innerHTML;
+  parent.appendChild(child);
 
-    elem.innerHTML = "";
-    elem.appendChild(parent);
+  elem.innerHTML = "";
+  elem.appendChild(parent);
 });
-
 
 // script end
 
 // gsap start
 var tl = gsap.timeline();
 
-tl
-    .from(".child span", {
-        x: 100,
-        stagger: .05,
-        duration: 0.75,
-        delay: 1,
-        ease: Power3.easeInOut,
-    })
-    .to(".parent .child", {
-        y: "-103%",
-        duration: 0.5,
-        ease: Circ.easeInOut
-    })
-    .to("#loader", {
-        height: 0,
-        duration: 1,
-        ease: Expo.easeInOut
-    })
-    .to("#green", {
-        height: '100%',
-        duration: 1,
-        delay: -1,
-        ease: Expo.easeInOut
-    })
-    .to("#home", {
-        height: '100vh',
-        duration: 0.65,
-        delay: -0.65,
-        ease: Expo.easeInOut
-    })
-    .to(".parentUp .childUp", {
-        height:'20vh',
-        duration: 0.75,
-        ease: Expo.easeInOut
-    })
+tl.from(".child span", {
+  x: 100,
+  stagger: 0.05,
+  duration: 0.75,
+  delay: 1,
+  ease: Power3.easeInOut,
+})
+  .to(".parent .child", {
+    y: "-103%",
+    duration: 0.5,
+    ease: Circ.easeInOut,
+  })
+  .to("#loader", {
+    height: 0,
+    duration: 1,
+    ease: Expo.easeInOut,
+  })
+  .to("#green", {
+    height: "100%",
+    duration: 1,
+    delay: -1,
+    ease: Expo.easeInOut,
+  })
+  .to("#home", {
+    height: "100vh",
+    duration: 0.65,
+    delay: -0.65,
+    ease: Expo.easeInOut,
+  })
+  .to(".parentUp .childUp", {
+    height: "20vh",
+    duration: 0.75,
+    ease: Expo.easeInOut,
+  });
 
-    
 // gsap end
 gsap.registerPlugin(ScrollTrigger);
 
@@ -138,7 +142,15 @@ const pageContainer = document.querySelector(".container");
 /* SMOOTH SCROLL */
 const scroller = new LocomotiveScroll({
   el: pageContainer,
-  smooth: true
+  smooth: true,
+  mobile: {
+    smooth: true,
+    breakpoint: 0
+  },
+  tablet: {
+    smooth: true,
+    breakpoint: 0
+  },
 });
 
 scroller.on("scroll", ScrollTrigger.update);
@@ -154,10 +166,10 @@ ScrollTrigger.scrollerProxy(pageContainer, {
       left: 0,
       top: 0,
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
     };
   },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
+  pinType: pageContainer.style.transform ? "transform" : "fixed",
 });
 
 ////////////////////////////////////
@@ -177,10 +189,10 @@ window.addEventListener("load", function () {
       pin: true,
       // anticipatePin: 1,
       start: "top top",
-      end: pinWrapWidth
+      end: pinWrapWidth,
     },
     x: -horizontalScrollLength,
-    ease: "none"
+    ease: "none",
   });
 
   ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
